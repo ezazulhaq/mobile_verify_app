e388591f8cbd3edeadc1a23a69b32fed821e7eb4
Ezazul Haq Abdul <ezazulhaq.haa@gmail.com>
2021-04-21 14:11:54 +0530

    Logout functionality implemented

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mobil_verify_app/screens/login_screen.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         child: null,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _auth.signOut();
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => LoginScreen()));
+        },
+        child: Icon(Icons.logout),
       ),
     );
   }
